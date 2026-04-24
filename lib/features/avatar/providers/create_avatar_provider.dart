6849a3d3f4dd870e5351d1c04a3dd56afe34ea02
mohidmentorsol <mohid.h@mentorsol.com>
2026-04-24 18:26:17 +0500

    feat : create avatar method

--- lib/features/avatar/providers/create_avatar_provider.dart
+++ lib/features/avatar/providers/create_avatar_provider.dart
@@ -1,12 +1,21 @@
+import 'package:avatar_flow/core/utils/toast_utils.dart';
+import 'package:avatar_flow/features/avatar/models/avatar_model.dart';
+import 'package:avatar_flow/features/avatar/repo/avatar_repo.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class CreateAvatarProvider extends ChangeNotifier {
+  final AvatarRepo _repo = AvatarRepo();
+
   int currentIndex = 0;
   String avatarName = "Lilian";
   String selectedGender = "Female";
   List<String> traits = ["Adventurous", "Charismatic"];
   String selectedVoice = "Voice 1";
   String prompt = "";
+  bool _isCreating = false;
+
+  bool get isCreating => _isCreating;
 
   void changeIndex(int index) {
     currentIndex = index;
@@ -45,5 +54,33 @@ class CreateAvatarProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  // create avatar
+  Future<void> createAvatar() async {
+    if (avatarName.trim().isEmpty) {
+      ToastUtils.error('Please enter a name for your avatar');
+      return;
+    }
+
+    _isCreating = true;
+    notifyListeners();
+
+    try {
+      final avatar = AvatarModel(
+        name: avatarName.trim(),
+        gender: selectedGender,
+        traits: traits,
+        avatarUrl: '', // null for now
+        voiceUrl: null, // null for now
+        userId: Supabase.instance.client.auth.currentUser!.id
+            .toString(), // repo will set this
+      );
+
+      await _repo.createAvatar(avatar);
+      ToastUtils.success('Avatar created successfully!');
+    } catch (e) {
+      ToastUtils.error('Failed to create avatar: $e');
+    } finally {
+      _isCreating = false;
+      notifyListeners();
+    }
+  }
 }
