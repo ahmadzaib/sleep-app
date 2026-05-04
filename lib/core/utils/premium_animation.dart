74f30a179bfca06ae5ceadf8c95914f94aba1b93
mohidmentorsol <mohid.h@mentorsol.com>
2026-05-04 19:11:24 +0500

    current milestone preview

--- lib/core/utils/premium_animation.dart
+++ lib/core/utils/premium_animation.dart
@@ -1,127 +1,127 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
-// class PremiumAnimation extends StatelessWidget {
-//   final Widget child;
-//   final Duration delay;
-//   final Duration duration;
-//   final Offset slideOffset;
-//   final double beginOpacity;
-//   final double endOpacity;
-//   final double beginScale;
-//   final double endScale;
-//   final Curve curve;
+class PremiumAnimation extends StatelessWidget {
+  final Widget child;
+  final Duration delay;
+  final Duration duration;
+  final Offset slideOffset;
+  final double beginOpacity;
+  final double endOpacity;
+  final double beginScale;
+  final double endScale;
+  final Curve curve;
 
-//   const PremiumAnimation({
-//     super.key,
-//     required this.child,
-//     this.delay = Duration.zero,
-//     this.duration = const Duration(milliseconds: 600),
-//     this.slideOffset = const Offset(0, 0.1),
-//     this.beginOpacity = 0.0,
-//     this.endOpacity = 1.0,
-//     this.beginScale = 1.0,
-//     this.endScale = 1.0,
-//     this.curve = Curves.easeOutCubic,
-//   });
+  const PremiumAnimation({
+    super.key,
+    required this.child,
+    this.delay = Duration.zero,
+    this.duration = const Duration(milliseconds: 600),
+    this.slideOffset = const Offset(0, 0.1),
+    this.beginOpacity = 0.0,
+    this.endOpacity = 1.0,
+    this.beginScale = 1.0,
+    this.endScale = 1.0,
+    this.curve = Curves.easeOutCubic,
+  });
 
-//   /// standard fade in from bottom effect
-//   factory PremiumAnimation.fadeInUp({
-//     required Widget child,
-//     Duration delay = Duration.zero,
-//     Duration duration = const Duration(milliseconds: 600),
-//     double slideDistance = 30.0,
-//   }) {
-//     return PremiumAnimation(
-//       delay: delay,
-//       duration: duration,
-//       slideOffset: Offset(0, slideDistance / 100),
-//       child: child,
-//     );
-//   }
+  /// standard fade in from bottom effect
+  factory PremiumAnimation.fadeInUp({
+    required Widget child,
+    Duration delay = Duration.zero,
+    Duration duration = const Duration(milliseconds: 600),
+    double slideDistance = 30.0,
+  }) {
+    return PremiumAnimation(
+      delay: delay,
+      duration: duration,
+      slideOffset: Offset(0, slideDistance / 100),
+      child: child,
+    );
+  }
 
-//   /// fade in from top
-//   factory PremiumAnimation.fadeInDown({
-//     required Widget child,
-//     Duration delay = Duration.zero,
-//     Duration duration = const Duration(milliseconds: 600),
-//     double slideDistance = -30.0,
-//   }) {
-//     return PremiumAnimation(
-//       delay: delay,
-//       duration: duration,
-//       slideOffset: Offset(0, slideDistance / 100),
-//       child: child,
-//     );
-//   }
+  /// fade in from top
+  factory PremiumAnimation.fadeInDown({
+    required Widget child,
+    Duration delay = Duration.zero,
+    Duration duration = const Duration(milliseconds: 600),
+    double slideDistance = -30.0,
+  }) {
+    return PremiumAnimation(
+      delay: delay,
+      duration: duration,
+      slideOffset: Offset(0, slideDistance / 100),
+      child: child,
+    );
+  }
 
-//   /// standard fade in with scale
-//   factory PremiumAnimation.scaleIn({
-//     required Widget child,
-//     Duration delay = Duration.zero,
-//     Duration duration = const Duration(milliseconds: 600),
-//     double beginScale = 0.8,
-//   }) {
-//     return PremiumAnimation(
-//       delay: delay,
-//       duration: duration,
-//       beginScale: beginScale,
-//       slideOffset: Offset.zero,
-//       child: child,
-//     );
-//   }
+  /// standard fade in with scale
+  factory PremiumAnimation.scaleIn({
+    required Widget child,
+    Duration delay = Duration.zero,
+    Duration duration = const Duration(milliseconds: 600),
+    double beginScale = 0.8,
+  }) {
+    return PremiumAnimation(
+      delay: delay,
+      duration: duration,
+      beginScale: beginScale,
+      slideOffset: Offset.zero,
+      child: child,
+    );
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     var animation = child
-//         .animate(delay: delay)
-//         .fadeIn(duration: duration, begin: beginOpacity, curve: curve);
+  @override
+  Widget build(BuildContext context) {
+    var animation = child
+        .animate(delay: delay)
+        .fadeIn(duration: duration, begin: beginOpacity, curve: curve);
 
-//     if (slideOffset != Offset.zero) {
-//       animation = animation.slide(
-//         begin: slideOffset,
-//         end: Offset.zero,
-//         duration: duration,
-//         curve: curve,
-//       );
-//     }
+    if (slideOffset != Offset.zero) {
+      animation = animation.slide(
+        begin: slideOffset,
+        end: Offset.zero,
+        duration: duration,
+        curve: curve,
+      );
+    }
 
-//     if (beginScale != endScale) {
-//       animation = animation.scale(
-//         begin: Offset(beginScale, beginScale),
-//         end: Offset(endScale, endScale),
-//         duration: duration,
-//         curve: curve,
-//       );
-//     }
+    if (beginScale != endScale) {
+      animation = animation.scale(
+        begin: Offset(beginScale, beginScale),
+        end: Offset(endScale, endScale),
+        duration: duration,
+        curve: curve,
+      );
+    }
 
-//     return animation;
-//   }
-// }
+    return animation;
+  }
+}
 
-// /// A helper class to handle staggered animations in lists
-// class StaggeredListAnimation extends StatelessWidget {
-//   final List<Widget> children;
-//   final Duration interval;
-//   final Duration initialDelay;
+/// A helper class to handle staggered animations in lists
+class StaggeredListAnimation extends StatelessWidget {
+  final List<Widget> children;
+  final Duration interval;
+  final Duration initialDelay;
 
-//   const StaggeredListAnimation({
-//     super.key,
-//     required this.children,
-//     this.interval = const Duration(milliseconds: 100),
-//     this.initialDelay = Duration.zero,
-//   });
+  const StaggeredListAnimation({
+    super.key,
+    required this.children,
+    this.interval = const Duration(milliseconds: 100),
+    this.initialDelay = Duration.zero,
+  });
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       mainAxisSize: MainAxisSize.min,
-//       children: List.generate(children.length, (index) {
-//         return PremiumAnimation.fadeInUp(
-//           delay: initialDelay + (interval * index),
-//           child: children[index],
-//         );
-//       }),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: List.generate(children.length, (index) {
+        return PremiumAnimation.fadeInUp(
+          delay: initialDelay + (interval * index),
+          child: children[index],
+        );
+      }),
+    );
+  }
+}
--- lib/features/milestones/views/components/my_avatars_view.dart
+++ lib/features/milestones/views/components/my_avatars_view.dart
@@ -5,8 +5,10 @@ import 'package:avatar_flow/core/router/routes.dart';
 import 'package:avatar_flow/core/utils/spacing.dart';
 import 'package:avatar_flow/features/avatar/views/components/avatar_cards.dart';
 import 'package:avatar_flow/features/avatar/views/components/avtar_milestones_tile.dart';
+import 'package:avatar_flow/features/milestones/providers/milestones_provider.dart';
 import 'package:avatar_flow/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class MyAvatarsView extends StatelessWidget {
   const MyAvatarsView({super.key});
@@ -18,10 +20,17 @@ class MyAvatarsView extends StatelessWidget {
         Spacing.y(2),
         AvatarCards(),
         Spacing.y(2),
-        const AvtarMilestonesTile(
-          title: 'Create 10 avatars',
-          totalValue: 10,
-          completedValue: 8,
+        Consumer<MilestonesProvider>(
+          builder: (context, provider, _) {
+            final current = provider.currentMilestone;
+            if (current == null) return const SizedBox.shrink();
+
+            return AvtarMilestonesTile(
+              title: current.milestone.title,
+              totalValue: current.targetCount,
+              completedValue: current.currentCount,
+            );
+          },
         ),
         Spacing.y(2),
         Padding(
